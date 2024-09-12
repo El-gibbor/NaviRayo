f603814c1ba4635a0c23d1ea05d4c3aee89d5117
El-gibbor <chiagoziem11@yahoo.com>
2024-09-12 14:23:47 +0200

    useEffect function to handle guest signin

--- navi-rayo/src/App.jsx
+++ navi-rayo/src/App.jsx
@@ -1,7 +1,21 @@
 import Login from "./Pages/Login";
+import { useEffect, useState } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [visited, setVisited] = useState(false);
+  const [guestEmail, setGuestEmail] = useState();
+
+  useEffect(() => {
+    const visitedAs = localStorage.getItem("visitedAs");
+    const storedGuestEmail = localStorage.getItem("guestEmailCode");
+    if (visitedAs) {
+      setVisited(true);
+    }
+    if (storedGuestEmail) {
+      setGuestEmail(storedGuestEmail)
+    }
+  }, []);
   return (
     <>
       <HashRouter>
