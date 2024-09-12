d5a87602f806bad54b8e8fc62837e9d11f2ca341
El-gibbor <chiagoziem11@yahoo.com>
2024-09-12 14:20:31 +0200

    implemented route path to the login page

--- navi-rayo/src/App.jsx
+++ navi-rayo/src/App.jsx
@@ -1,6 +1,17 @@
+import Login from "./Pages/Login";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
+  return (
+    <>
+      <HashRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/*" element={<PageError />} /> */}
+        </Routes>
+      </HashRouter>
+    </>
+  );
 }
 
 export default App;
