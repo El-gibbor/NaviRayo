782dfe1ba40f8d615535862439172ac8792a6b58
El-gibbor <chiagoziem11@yahoo.com>
2024-09-14 12:02:09 +0200

    added rounded styles to form input box

--- navirayo/src/Pages/Login.jsx
+++ navirayo/src/Pages/Login.jsx
@@ -48,12 +48,10 @@ function Login() {
 
   return (
     <div className="h-svh w-full overflow-x-clip overflow-y-auto flex items-center justify-between max-sm:justify-end p-8 max-sm:p-4 bg-white dark:bg-body-color-dark">
-      {/* Helmet */}
       <Helmet>
         <title>Login | gerayo.</title>
       </Helmet>
       <div className="fixed top-0 left-0 p-8 max-[250px]:bg-white w-full h-fit flex justify-end items-center select-none ">
-        {/* Skip */}
         <button
           onClick={LoginGuest}
           className="outline-none flex items-center justify-start text-main-color cursor-pointer h-fit w-fit active:scale-95 transition "
@@ -65,7 +63,7 @@ function Login() {
             </>
           ) : (
             <>
-              <p className=" font-normaltext-sm ">Continue as Guest</p>
+              <p className=" font-normal text-sm ">Continue as Guest</p>
               <GrFormNext />
             </>
           )}
@@ -79,9 +77,9 @@ function Login() {
           className="w-full flex flex-col items-center justify-center"
         > */}
         <div className="flex flex-col items-center justify-center w-full h-fit max-sm:mt-10">
-          <div className="flex items-center justify-center py-3">
+          <div className="flex items-center justify-center py-1">
             <MdLocationOn className="text-[30px] text-main-color pb-[1px] translate-x-1" />
-            <h1 className=" font-bold text-[25px] tracking-tighter text-main-color pr-3  ">
+            <h1 className=" font-bold text-[25px] tracking-tighter text-main-color pr-3 ">
               NaviRayo
             </h1>
           </div>
@@ -94,7 +92,7 @@ function Login() {
               <input
                 type="text"
                 name=""
-                className="bg-[#F2F1F6] dark:bg-container-dark-2 text-dark-text dark:text-white text-sm  h-[40px] w-full flex items-center justify-start px-5 outline-none focus:ring-2 ring-main-color ring-offset-2 dark:ring-offset-body-color-dark dark:placeholder:text-light-text"
+                className="bg-[#F2F1F6] dark:bg-container-dark-2 text-dark-text dark:text-white text-sm rounded-sm h-[40px] w-full flex items-center justify-start px-5 outline-none focus:ring-2 ring-main-color ring-offset-2 dark:ring-offset-body-color-dark dark:placeholder:text-light-text"
                 placeholder="Your Email"
               />
             </div>
@@ -105,7 +103,7 @@ function Login() {
               <input
                 type="password"
                 id="password"
-                className="bg-[#F2F1F6] dark:bg-container-dark-2 text-dark-text dark:text-white text-sm  h-[40px] w-full flex items-center justify-start px-5 outline-none focus:ring-2 ring-main-color ring-offset-2 dark:ring-offset-body-color-dark dark:placeholder:text-light-text"
+                className="bg-[#F2F1F6] dark:bg-container-dark-2 rounded-sm text-dark-text dark:text-white text-sm  h-[40px] w-full flex items-center justify-start px-5 outline-none focus:ring-2 ring-main-color ring-offset-2 dark:ring-offset-body-color-dark dark:placeholder:text-light-text"
                 placeholder="Password"
               />
             </div>
@@ -127,7 +125,7 @@ function Login() {
         <div className="flex flex-col items-center justify-start w-full">
           <button
             // onClick={handleClick}
-            className="bg-main-color my-4 outline-none text-white font-bold text-base transition active:scale-90 h-[40px] w-full max-w-[350px] flex items-center justify-center gap-2"
+            className="bg-main-color my-4 outline-none text-white rounded-sm font-bold text-base transition active:scale-90 h-[40px] w-full max-w-[350px] flex items-center justify-center gap-2"
           >
             {pending ? (
               <>
