e29812437ab1d193f86f1e7a7be135ce10e533d5
El-gibbor <chiagoziem11@yahoo.com>
2024-09-15 20:41:47 +0200

    added a celebration image to modal display

--- navirayo/src/Components/Welcome.jsx
+++ navirayo/src/Components/Welcome.jsx
@@ -31,7 +31,7 @@ function Welcome() {
 
       {/* Modal content */}
       <div
-        className={`fixed top-0 left-0 right-0 bottom-0 m-auto flex items-center justify-end flex-col shadow-2xl shadow-stone-500/20 bg-white overflow-clip text-dark-text w-[80%] rounded-3xl max-w-[380px] h-[90%] max-h-[360px] p-8 transition-all ease-in-out duration-300 ${
+        className={`fixed top-0 left-0 right-0 bottom-0 m-auto flex items-center justify-end flex-col shadow-2xl shadow-stone-500/20 bg-white overflow-clip text-dark-text w-[80%] rounded-md max-w-[380px] h-[90%] max-h-[360px] p-8 transition-all ease-in-out duration-300 ${
           animate ? "scale-100 opacity-100 visible z-[60]" : "scale-75 invisible opacity-0 -z-10"
         }`}
       >
@@ -44,7 +44,7 @@ function Welcome() {
         </p>
         <button
           onClick={hideWelcomeModal}
-          className="h-[45px] min-h-[45px] shadow-lg overflow-clip tracking-tight w-full max-w-[280px] mt-5 bg-main-color text-white font-semibold text-sm rounded-xl flex items-center justify-center gap-1"
+          className="h-[45px] min-h-[45px] shadow-lg overflow-clip tracking-tight w-full max-w-[280px] mt-5 bg-main-color text-white font-semibold text-sm rounded-md flex items-center justify-center gap-1"
         >
           {saving ? (
             <CgSpinner className="animate-spinLoader text-3xl" />
--- navirayo/src/Pages/Track.jsx
+++ navirayo/src/Pages/Track.jsx
@@ -1,38 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import Welcome from "../Components/Welcome";
 
 
 function Track({ guestEmail }) {
-    const [visited, setVisited] = useState(false);
-    const navigate = useNavigate();
-    const { pathname } = useLocation();
-    const [loading, setLoading] = useState(true);
+  const [visited, setVisited] = useState(false);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-        document.documentElement.classList.remove("no-scroll");
-      }, [pathname]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.documentElement.classList.remove("no-scroll");
+  }, [pathname]);
 
-    useEffect(() => {
-        const visitadAs = localStorage.getItem("visitedAs");
-        if (!visitadAs) {
-          navigate("/login");
-        }
-      }, []);
+  useEffect(() => {
+    const visitadAs = localStorage.getItem("visitedAs");
+    if (!visitadAs) {
+      navigate("/login");
+    }
+  }, []);
 
-      useEffect(() => {
-        setTimeout(() => {
-          setLoading(false);
-        }, 500);
-      }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
 
-      // check if this user has visited before
-      useEffect(() => {
-        const visitedBefore = localStorage.getItem("visitedGerayo");
-        if (visitedBefore) {
-          setVisited(true);
-        }
-      }, []);
+  // check if this user has visited before
+  useEffect(() => {
+    const visitedBefore = localStorage.getItem("visitedGerayo");
+    if (visitedBefore) {
+      setVisited(true);
+    }
+  }, []);
+
+  return (
+    <div className="bg-stone-100 dark:bg-body-color-dark min-h-svh max-md:pb-10 text-dark-text">
+      {/* Welcome */}
+      {!visited && <Welcome />}
+    </div>
+  )
 }
 
 export default Track;
