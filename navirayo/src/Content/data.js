0d6cb169a8ae8fd35d0323471c6c258b50c211ff
El-gibbor <chiagoziem11@yahoo.com>
2024-09-16 14:08:36 +0200

    added an array of FAQs

---
+++ navirayo/src/Content/data.js
@@ -0,0 +1,38 @@
+// Faqs
+export const Faqs = [
+    {
+      question: "How do I purchase a bus ticket online?",
+      answer:
+        "To purchase a bus ticket online, visit our website or mobile app, select your desired route and travel date, choose the available bus, and complete the payment process using your preferred payment method. You will receive a digital ticket via email or SMS.",
+    },
+    {
+      question: "Can I track the location of my bus in real-time?",
+      answer:
+        "Yes, our system allows you to track the location of your bus in real-time. Simply log in to your account on our website or mobile app, go to the 'Track My Bus' section, and enter your ticket number or bus details to view its current location and estimated arrival time.",
+    },
+    {
+      question: "What should I do if I miss my bus?",
+      answer:
+        "If you miss your bus, please contact our customer support team as soon as possible. Depending on availability and the terms and conditions of your ticket, we may be able to rebook you on the next available bus or offer a partial refund.",
+    },
+    {
+      question: "Is it safe to make payments online for bus tickets?",
+      answer:
+        "Yes, making payments online through our system is safe and secure. We use advanced encryption technologies and comply with international security standards to ensure that your personal and payment information is protected.",
+    },
+    {
+      question: "How can I cancel or change my ticket booking?",
+      answer:
+        "To cancel or change your ticket booking, log in to your account on our website or mobile app, go to the 'My Bookings' section, select the ticket you wish to cancel or change, and follow the prompts. Please note that cancellations and changes are subject to our terms and conditions, and additional fees may apply.",
+    },
+    {
+      question: "What should I do if I lose my digital ticket?",
+      answer:
+        "If you lose your digital ticket, you can retrieve it by logging into your account on our website or mobile app. Go to the 'My Bookings' section to view and re-send your ticket details.",
+    },
+    {
+      question: "Can I bring luggage on the bus?",
+      answer:
+        "Yes, you can bring luggage on the bus. Each passenger is allowed one piece of checked luggage and one carry-on bag. Additional luggage may incur extra fees, depending on the bus company’s policies.",
+    },
+  ];
