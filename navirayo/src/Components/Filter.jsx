bb0636c0bcd806e48499258c2f762f5d6611e65b
El-gibbor <chiagoziem11@yahoo.com>
2024-09-16 14:05:37 +0200

    Filter component for bus search functionality

---
+++ navirayo/src/Components/Filter.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+function Filter({ onFilterSubmit, onRouteFilter, mobileSearch }) {
+    const [inputFrom, setInputFrom] = useState(false);
+    const [inputValueFrom, setInputValueFrom] = useState("");
+    const [inputTo, setInputTo] = useState(false);
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+export default Filter;
