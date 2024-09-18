b51a5e41ad0490f099e16e8394ffd409e15cd76c
El-gibbor <chiagoziem11@yahoo.com>
2024-09-18 14:08:31 +0200

    readjusted some styling

--- navirayo/src/Pages/Login.jsx
+++ navirayo/src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { GrFormNext } from "react-icons/gr";
 import { Link, useNavigate } from "react-router-dom";
-// import { Fade } from "react-awesome-reveal";
+import { Fade } from "react-awesome-reveal";
 import { CgSpinner } from "react-icons/cg";
 import { Helmet } from "react-helmet";
 import { MdLocationOn } from "react-icons/md";
@@ -48,10 +48,12 @@ function Login() {
 
   return (
     <div className="h-svh w-full overflow-x-clip overflow-y-auto flex items-center justify-between max-sm:justify-end p-8 max-sm:p-4 bg-white dark:bg-body-color-dark">
+      {/* Helmet */}
       <Helmet>
         <title>Login | gerayo.</title>
       </Helmet>
       <div className="fixed top-0 left-0 p-8 max-[250px]:bg-white w-full h-fit flex justify-end items-center select-none ">
+        {/* Skip */}
         <button
           onClick={LoginGuest}
           className="outline-none flex items-center justify-start text-main-color cursor-pointer h-fit w-fit active:scale-95 transition "
@@ -63,7 +65,7 @@ function Login() {
             </>
           ) : (
             <>
-              <p className=" font-normal text-sm ">Continue as Guest</p>
+              <p className=" font-medium text-sm ">Continue as Guest</p>
               <GrFormNext />
             </>
           )}
@@ -77,33 +79,37 @@ function Login() {
           className="w-full flex flex-col items-center justify-center"
         > */}
         <div className="flex flex-col items-center justify-center w-full h-fit max-sm:mt-10">
-          <div className="flex items-center justify-center py-1">
+          <div className="flex items-center justify-center py-3">
             <MdLocationOn className="text-[30px] text-main-color pb-[1px] translate-x-1" />
-            <h1 className=" font-bold text-[25px] tracking-tighter text-main-color pr-3 ">
-              NaviRayo
+            <h1 className=" font-bold text-[25px] tracking-tighter text-main-color pr-3  ">
+              gerayo.
             </h1>
           </div>
-          <h1 className="font-normal tracking-tighter text-2xl text-left text-dark-text/90 dark:text-white">
+          <h1 className="font-bold tracking-tighter text-2xl text-left text-dark-text/90 dark:text-white">
             Let's Sign you in.
           </h1>
+          <p className="text-sm text-left text-dark-text/80 dark:text-light-text  max-w-[400px] font-medium">
+            Welcome back!
+          </p>
+
           <form className="w-full pt-3 flex flex-col items-center justify-center gap-2">
             <div className="flex flex-col w-full items-start justify-start max-w-[350px]">
-              <p className="text-sm font-normalpy-1 text-dark-text dark:text-light-text">Email</p>
+              <p className="text-sm font-medium py-1 text-dark-text dark:text-light-text">Email</p>
               <input
                 type="text"
                 name=""
-                className="bg-[#F2F1F6] dark:bg-container-dark-2 text-dark-text dark:text-white text-sm rounded-sm h-[40px] w-full flex items-center justify-start px-5 outline-none focus:ring-2 ring-main-color ring-offset-2 dark:ring-offset-body-color-dark dark:placeholder:text-light-text"
+                className="bg-[#F2F1F6] dark:bg-container-dark-2 text-dark-text dark:text-white text-sm  h-[40px] w-full flex items-center justify-start px-5 outline-none focus:ring-2 ring-main-color ring-offset-2 dark:ring-offset-body-color-dark dark:placeholder:text-light-text  rounded-full "
                 placeholder="Your Email"
               />
             </div>
             <div className="flex flex-col w-full items-start justify-start max-w-[350px]">
-              <p className="text-sm font-normal py-1 text-dark-text dark:text-light-text">
+              <p className="text-sm font-medium py-1 text-dark-text dark:text-light-text">
                 Password
               </p>
               <input
                 type="password"
                 id="password"
-                className="bg-[#F2F1F6] dark:bg-container-dark-2 rounded-sm text-dark-text dark:text-white text-sm  h-[40px] w-full flex items-center justify-start px-5 outline-none focus:ring-2 ring-main-color ring-offset-2 dark:ring-offset-body-color-dark dark:placeholder:text-light-text"
+                className="bg-[#F2F1F6] dark:bg-container-dark-2 text-dark-text dark:text-white text-sm  h-[40px] w-full flex items-center justify-start px-5 outline-none focus:ring-2 ring-main-color ring-offset-2 dark:ring-offset-body-color-dark dark:placeholder:text-light-text  rounded-full "
                 placeholder="Password"
               />
             </div>
@@ -115,7 +121,7 @@ function Login() {
                   className=" accent-main-color h-4 w-4"
                   onClick={handleShowPassword}
                 />
-                <p className="text-dark-text dark:text-light-text font-normal text-sm capitalize">
+                <p className="text-dark-text dark:text-light-text font-medium text-sm capitalize">
                   Show password
                 </p>
               </label>
@@ -125,7 +131,7 @@ function Login() {
         <div className="flex flex-col items-center justify-start w-full">
           <button
             // onClick={handleClick}
-            className="bg-main-color my-4 outline-none text-white rounded-sm font-bold text-base transition active:scale-90 h-[40px] w-full max-w-[350px] flex items-center justify-center gap-2"
+            className="bg-main-color my-4 outline-none text-white font-bold text-base transition active:scale-90 h-[40px] w-full max-w-[350px] rounded-full flex items-center justify-center gap-2"
           >
             {pending ? (
               <>
@@ -136,7 +142,7 @@ function Login() {
               <>Sign in</>
             )}
           </button>
-          <p className="flex items-center justify-center w-full font-normal text-sm gap-2 text-dark-text dark:text-light-text ">
+          <p className="flex items-center justify-center w-full text-sm gap-2 text-dark-text dark:text-light-text ">
             Don't have an account?{" "}
             <Link to={"/register"} className="text-main-color font-semibold">
               Register
@@ -150,4 +156,3 @@ function Login() {
 }
 
 export default Login;
-
